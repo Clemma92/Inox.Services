0643eb71dd2ec97fd842d10177db28118c7a70db
Clemma92 <simone.clementoni@gmail.com>
2025-04-15 19:46:22 +0200

    Prova 6

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let transitioned = false;
 const video = document.querySelector('video');
 const sfondo = document.querySelector('.sfondo');
 // function adjustSfondo() {
@@ -20,7 +21,7 @@ const sfondo = document.querySelector('.sfondo');
 function SlittaBox(numero, box){
     if (numero < 0 || numero >= box.length) return;
     box[numero].style.transform = 'translateX(0)';
-    setTimeout(() =>{if (numero === box.length - 1) {document.querySelector('button').classList.add('mostra');}},800);
+    setTimeout(() =>{if (numero === box.length - 1) {document.querySelector('.btn').classList.add('mostra');}},800);
 }
 function CambiaSfondo(callback) {
     let box = document.querySelectorAll('.box-testo');
@@ -33,8 +34,43 @@ function CambiaSfondo(callback) {
             }
         }, {once: true});          
     }
+    transitioned = true;
+}
+
+function animateOut(){
+    let navBar = document.querySelector('nav');
+    let boxContainer = document.querySelectorAll('.box-testo');
+    if (transitioned===true){
+        boxContainer.forEach((boxContainer) =>{boxContainer.style.transition= 'transform 0.4s ease-in-out, scale 0.3s ease-in-out'});
+        boxContainer.forEach((boxContainer) =>{boxContainer.style.transform='translateX(-5vw)'});
+        boxContainer[0].addEventListener('transitionend', () => {
+
+            boxContainer.forEach((boxContainer) =>{
+                boxContainer.style.transform='translateX(10vw)'
+            });
+            setTimeout(()=>{
+                navBar.style.transform='translate(-80vw)';
+
+            },150);
+
+        },{once:true});
+        boxContainer.forEach((boxContainer) =>{
+            boxContainer.style.transition= 'transform 0.8s ease-in-out, scale 0.3s ease-in-out'
+        });
+        transitioned=false;
+    }
+    else{
+            for (let i=0; i<boxContainer.length; i++){
+                setTimeout(() =>SlittaBox(i, boxContainer),400 * i);
+            }
+            navBar.style.transform='translate(-100vw)';
+            sfondo.addEventListener('click', ()=>{navBar.style.transform='translate(-100vw)';});
+            transitioned=true;
+        }
 }
 
 video.addEventListener('ended', () => CambiaSfondo(SlittaBox));
+document.querySelector('.menu-btn').addEventListener('click', animateOut);
+
 
 
