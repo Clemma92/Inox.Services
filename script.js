51ee6511a12a362d389f3bea957de6ed1bf8062a
Clemma92 <simone.clementoni@gmail.com>
2025-04-15 16:33:25 +0200

    Prova 3

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 const video = document.querySelector('video');
 const sfondo = document.querySelector('.sfondo');
-function adjustSfondo() {
-    if(window.matchMedia("(min-width: 1024px)").matches){
-        const navbar = document.querySelector('nav');
-        const navbarHeight = navbar.offsetHeight;
-        const viewportHeight = document.documentElement.clientHeight;
-        sfondo.style.top = `${navbarHeight}px`;
-        sfondo.style.height = `${viewportHeight - navbarHeight}px`;
-    }
-    else{
-        const navbar = document.querySelector('header');
-        const navbarHeight = navbar.offsetHeight;
-        const viewportHeight = document.documentElement.clientHeight;
-        sfondo.style.top = `${navbarHeight}px`;
-        sfondo.style.height = `${viewportHeight - navbarHeight}px`;
-    }
-}
+// function adjustSfondo() {
+//     if(window.matchMedia("(min-width: 1024px)").matches){
+//         const navbar = document.querySelector('nav');
+//         const navbarHeight = navbar.offsetHeight;
+//         const viewportHeight = document.documentElement.clientHeight;
+//         sfondo.style.top = `${navbarHeight}px`;
+//         sfondo.style.height = `${viewportHeight - navbarHeight}px`;
+//     }
+//     else{
+//         const navbar = document.querySelector('header');
+//         const navbarHeight = navbar.offsetHeight;
+//         const viewportHeight = document.documentElement.clientHeight;
+//         sfondo.style.top = `${navbarHeight}px`;
+//         sfondo.style.height = `${viewportHeight - navbarHeight}px`;
+//     }
+// }
+
 function SlittaBox(numero, box){
     if (numero < 0 || numero >= box.length) return;
     box[numero].style.transform = 'translateX(0)';
@@ -33,8 +34,7 @@ function CambiaSfondo(callback) {
         }, {once: true});          
     }
 }
-window.addEventListener('load', adjustSfondo);
-window.addEventListener('resize', adjustSfondo);
+
 video.addEventListener('ended', () => CambiaSfondo(SlittaBox));
 
 
